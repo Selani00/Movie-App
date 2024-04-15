c743ba136629f5f2316f846fa6eaf36b960e6794
Selani00 <pkselani00@gmail.com>
2024-04-15 17:31:56 +0530

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum accusamus? Inventore ea quis veritatis accusantium illum, vitae voluptatum rerum facilis consectetur deserunt omnis ipsum, repudiandae qui. Voluptatem, officia veritatis?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, ipsa eaque. A sed incidunt necessitatibus quasi debitis impedit nemo quae natus nisi temporibus, quos eaque ratione, quidem excepturi voluptatibus sint.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum accusamus? Inventore ea quis veritatis accusantium illum, vitae voluptatum rerum facilis consectetur deserunt omnis ipsum, repudiandae qui. Voluptatem, officia veritatis?</p>
+      
+    </div>
+  )
+}
+
+export default page
